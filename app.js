93b761bbb63cb16eec501ffe2fb98da024acf87c
AhmadBilalAslam <ahmadbilal07@live.com>
2019-06-02 21:09:26 +0500

    Deletion finally working

--- app.js
+++ app.js
@@ -57,12 +57,14 @@ app.use(function(err, req, res, next) {
 });
 
 
-
-/*
+//
+//
+//checking
+//
 app.listen(process.env.PORT || 3000, function(){
 
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-*/
+
 module.exports = app;
 
